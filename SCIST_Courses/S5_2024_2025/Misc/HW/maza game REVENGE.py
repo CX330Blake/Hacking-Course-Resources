7300a5bff84481d9a7c2fdb33a0025552c534014
DESKTOP-RIVTRKI\user <timchen975@gmail.com>
2024-11-04 23:25:49 +0800

    .

--- SCIST_Courses/S5_2024_2025/Misc/HW/maza game REVENGE.py
+++ SCIST_Courses/S5_2024_2025/Misc/HW/maza game REVENGE.py
@@ -1,11 +1,9 @@
 from pwn import *
-import time
 
-# 連接到伺服器
 r = remote("lab.scist.org", 31013)
 
-# 定義方向的對應字符
 directions = {"up": b"W", "down": b"S", "left": b"A", "right": b"D"}
+moves = [(0, 1, "right"), (0, -1, "left"), (1, 0, "down"), (-1, 0, "up")]
 
 
 def read_maze():
@@ -25,10 +23,53 @@ def read_maze():
                 row_data.append(0)
         maze_matrix.append(row_data)
 
-    for row in maze_matrix:
-        print(row)
+    return maze_matrix
+
+
+def dfs(maze_matrix, start, end):
+    stack = [(start, [])]
+    visited = set()
+
+    while stack:
+        (x, y), path = stack.pop()
+        if (x, y) == end:
+            return path
+
+        visited.add((x, y))
+
+        for dx, dy, direction in moves:
+            nx, ny = x + dx, y + dy
+            if (
+                0 <= nx < len(maze_matrix)
+                and 0 <= ny < len(maze_matrix[0])
+                and (nx, ny) not in visited
+            ):
+                if maze_matrix[nx][ny] == 0 or maze_matrix[nx][ny] == "E":
+                    stack.append(((nx, ny), path + [direction]))
+    return None
+
+
+def find_positions(maze_matrix):
+    start = end = None
+    for i, row in enumerate(maze_matrix):
+        for j, cell in enumerate(row):
+            if cell == "P":
+                start = (i, j)
+            elif cell == "E":
+                end = (i, j)
+    return start, end
 
 
 if __name__ == "__main__":
-    read_maze()
-    r.close()
+    while True:
+        try:
+            maze_matrix = read_maze()
+            start, end = find_positions(maze_matrix)
+            if start and end:
+                path = dfs(maze_matrix, start, end)
+                if path:
+                    for move in path:
+                        r.sendline(directions[move])
+        except:
+            break
+    r.interactive()
